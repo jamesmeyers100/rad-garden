075494221d5416b1cb5c775607be6cdaab67c94e
jamesmeyers100 <jamesmeyers100@protonmail.com>
2021-12-24 03:08:44 -0600

    weather widget stub

---
+++ src/components/WeatherWidget/WeatherWidget.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./WeatherWidget.css";
+
+const WeatherWidget = () => {
+  return (
+    <div className="weatherWidget">
+      <h3>Temp:</h3>
+      <h3>Weather: </h3>
+    </div>
+  );
+};
+
+export default WeatherWidget;
