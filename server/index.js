3ea6671ae78c73f2cf285166ee9551d57bfa3148
jamesmeyers100 <jamesmeyers100@protonmail.com>
2021-12-24 15:37:18 -0600

    dotenv working in node variables

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const routes = require("./routes/garden");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.REACT_APP_PORT || 8000;
 
 const app = express();
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running.`);
+  console.log("this PORTenv", process.env.REACT_APP_PORT);
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Main from './pages/Main';
+import ReactDOM from "react-dom";
+import React from "react";
+import Main from "./pages/Main";
 
 const App = function () {
   return <Main />;
 };
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
